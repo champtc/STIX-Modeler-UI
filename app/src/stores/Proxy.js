1f45f0e4cdfe7ea2b745d67ff49a8734bb56b2ef
Dallas Phillips <dallasphillips24@gmail.com>
2021-05-20 12:56:35 -0700

    Filling in the Proxy stub with a post to Arkon through the Gateway. Still a WIP.

--- app/src/stores/Proxy.js
+++ app/src/stores/Proxy.js
@@ -1,10 +1,24 @@
+import axios from 'axios';
+
 class Proxy {
     constructor() {
-
+        this.apiGateway = process.env.API_GATEWAY;
+        this.apiKey = process.env.KONG_CONSUMER_API_KEY;
+        this.endpoint = process.env.TIPPING_ENDPOINT;
     }
 
-    submit(bundle) {
+    async submit(bundle) {
+        console.log('Submitting:')
         console.log(bundle);
+        console.log(`https://${this.apiGateway}${this.endpoint}`)
+
+        const response = await axios({
+            method: 'post',
+            url: `https://${this.apiGateway}${this.endpoint}`,
+            data: { bundle },
+            headers: {'Api-Key': this.apiKey, 'Content-Type': 'application/vnd.dl.tipping.submission+json;version=1'}
+        });
+        console.log(`Received code: ${response.status}`);
     }
 }
 
