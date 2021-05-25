fb8257fe0f84b570e90025b7989b1f414f42a8ce
Michael Pope <michaelpopedeveloper@gmail.com>
2021-05-25 15:07:24 -0700

    Updated .env variable names

--- app/src/stores/Proxy.js
+++ app/src/stores/Proxy.js
@@ -2,21 +2,20 @@ import axios from 'axios';
 
 class Proxy {
     constructor() {
-        this.apiGateway = process.env.API_GATEWAY;
-        this.apiKey = process.env.KONG_CONSUMER_API_KEY;
-        this.endpoint = process.env.TIPPING_ENDPOINT;
+        this.url = process.env.URL;
+        this.endpoint = process.env.ENDPOINT;
     }
 
     async submit(bundle) {
         console.log('Submitting:')
         console.log(bundle);
-        console.log(`https://${this.apiGateway}${this.endpoint}`)
+        console.log(`https://${this.url}${this.endpoint}`)
 
         const response = await axios({
             method: 'post',
-            url: `https://${this.apiGateway}${this.endpoint}`,
+            url: `https://${this.url}${this.endpoint}`,
             data: { bundle },
-            headers: {'Api-Key': this.apiKey, 'Content-Type': 'application/vnd.dl.tipping.submission+json;version=1'}
+            headers: { 'Content-Type': 'application/vnd.dl.tipping.submission+json;version=1' }
         });
         console.log(`Received code: ${response.status}`);
     }
