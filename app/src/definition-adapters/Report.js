0cdd2bbff3cad5f3e90d1b2edc2e9471c8553fd0
CyberDaedalus00 <Paul.Patrick@daedafusion.com>
2021-06-07 15:01:18 -0400

    fix syntax error in report.js

--- app/src/definition-adapters/Report.js
+++ app/src/definition-adapters/Report.js
@@ -2,40 +2,54 @@ import common from '../definitions/common.json';
 import rawDefinition from '../definitions/report.json';
 import deepmerge from 'deepmerge';
 
-import {Base} from './Base';
+import { Base } from './Base';
 
-class Report extends Base  {
+class Report extends Base {
+  constructor() {
+    const definition_extension = {
+      img: 'report.png',
+      prefix: 'report--',
+      active: true,
+      relationships: [
+        { type: 'references', target: 'identity', x_embed: 'object_refs' },
+        { type: 'references', target: 'sighting', x_embed: 'object_refs' },
+        { type: 'references', target: 'observed-data', x_embed: 'object_refs' },
+        { type: 'references', target: 'indicator', x_embed: 'object_refs' },
+        { type: 'references', target: 'malware', x_embed: 'object_refs' },
+        { type: 'references', target: 'report', x_embed: 'object_refs' },
+        {
+          type: 'references',
+          target: 'attack-pattern',
+          x_embed: 'object_refs'
+        },
+        { type: 'references', target: 'threat-actor', x_embed: 'object_refs' },
+        { type: 'references', target: 'intrusion-set', x_embed: 'object_refs' },
+        { type: 'references', target: 'campaign', x_embed: 'object_refs' },
+        {
+          type: 'references',
+          target: 'course-of-action',
+          x_embed: 'object_refs'
+        },
+        { type: 'references', target: 'tool', x_embed: 'object_refs' },
+        {
+          type: 'created-by',
+          target: 'identity',
+          x_exclusive: true,
+          x_embed: 'created_by_ref'
+        },
+        { type: 'references', target: 'vulnerability', x_embed: 'object_refs' }
+      ]
+    };
 
-    constructor() {
-        const definition_extension = {
-            "img": "report.png",
-            "prefix": "report--",
-            "active": true,
-            "relationships": [
-                {"type": "references", "target": "identity", "x_embed": "object_refs"},
-                {"type": "references", "target": "sighting", "x_embed": "object_refs"},
-                {"type": "references", "target": "observed-data", "x_embed": "object_refs"},
-                {"type": "references", "target": "indicator", "x_embed": "object_refs"},
-                {"type": "references", "target": "malware", "x_embed": "object_refs"},
-                {"type": "references", "target": "report", "x_embed": "object_refs"},
-                {"type": "references", "target": "attack-pattern", "x_embed": "object_refs"},
-                {"type": "references", "target": "threat-actor", "x_embed": "object_refs"},
-                {"type": "references", "target": "intrusion-set", "x_embed": "object_refs"},
-                {"type": "references", "target": "campaign", "x_embed": "object_refs"},
-                {"type": "references", "target": "course-of-action", "x_embed": "object_refs"},
-                {"type": "references", "target": "tool", "x_embed": "object_refs"},
-                {"type": "created-by", "target": "identity", "x_exclusive": true, "x_embed": "created_by_ref"},
-                {"type": "references", "target": "vulnerability", "x_embed": "object_refs"},
-            ]
-        }
+    let def = deepmerge(definition_extension, rawDefinition);
 
-        let def = deepmerge(definition_extension, rawDefinition);
+    super(common, def);
 
-        super(common, def);
-
-        this.properties["object_refs"].control = "hidden";
-        this.properties["report_types"].vocab = this.definitions["report-type-ov"].enum;
-    }
+    this.properties['object_refs'].control = 'hidden';
+    this.properties['report_types'].vocab = this.definitions[
+      'report-type-ov'
+    ].enum;
+  }
 }
 
 const singleton = new Report();
