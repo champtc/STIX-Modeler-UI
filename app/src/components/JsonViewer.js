38dc7b6b0ee6d8046984ebab3037b4ec510ff97e
Michael Pope <michaelpopedeveloper@gmail.com>
2021-06-30 13:33:30 -0700

    Created logic to change bundle object date values to ISO date format

--- app/src/components/JsonViewer.js
+++ app/src/components/JsonViewer.js
@@ -13,6 +13,7 @@ export default class JsonViewer extends React.Component {
     super(props);
 
     this.onClickCopyJSONHandler = this.onClickCopyJSONHandler.bind(this);
+    this.toISODate = this.toISODate.bind(this);
   }
 
   onClickCopyJSONHandler() {
@@ -31,10 +32,27 @@ export default class JsonViewer extends React.Component {
     this.props.onClickShowGrowlHandler(message);
   }
 
+  toISODate(bundleParentObject) {
+    const cloneBundleParentObject = { ...bundleParentObject };
+    cloneBundleParentObject.objects = bundleParentObject.objects.map(obj => {
+      console.log(' OBJ: ', obj);
+      let bundleObj = {};
+      Object.entries(obj).map(([key, value]) => (Date.parse(value)) ?
+      bundleObj[key] = (new Date(value).getUTCMilliseconds()) ? new Date(value).toISOString() : new Date(value).toISOString().split('.')[0]+"Z" 
+        : bundleObj[key] = value);
+      return bundleObj;
+    })
+    return cloneBundleParentObject;
+  }
+
   render() {
     console.log(
       'Bundle: ',
       JSON.stringify(removeEmptyValues(this.props.json), null, 2)
+    );
+    console.log(
+      'Bundle Iso Dates: ',
+      JSON.stringify(this.toISODate(removeEmptyValues(this.props.json)), null, 2)
     );
     return (
       <Panel
@@ -45,7 +63,7 @@ export default class JsonViewer extends React.Component {
           <div className="json-content">
             <pre id="json-content">
               {this.props.json &&
-                JSON.stringify(removeEmptyValues(this.props.json), null, 2)}
+                JSON.stringify(this.toISODate(removeEmptyValues(this.props.json)), null, 2)}
             </pre>
           </div>
 
